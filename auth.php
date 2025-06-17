e049c48e405f81f629026006f4e7b3590476191f
Graciel-Jacinto <gracieljacintojaime85@gmail.com>
2025-06-17 22:27:19 +0200

    Atualizando o Dasboard , ATUALIZANO API DE TEMPERATURA E COLOCANDO HORA E DATA NO SISTEMA E A PAGINA DE PEDIDO EM MANUTECAO

--- auth.php
+++ auth.php
@@ -7,12 +7,4 @@ if (!isset($_SESSION['usuario'])) {
     exit();
 }
 
-// Você pode adicionar verificações adicionais aqui, como:
-// - Verificar se o tipo de usuário tem acesso à página
-// - Verificar se a sessão ainda é válida
-// - Registrar atividade do usuário
-
-// Exemplo de uso em outras páginas:
-// require_once 'auth.php';
-// Agora você pode acessar $_SESSION['usuario']['nome'], etc.
 ?>
